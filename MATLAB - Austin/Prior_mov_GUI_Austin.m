59acd82f3506cf40b55ca85bdf3601c03e0b4968
arparrish <arparrish1@gmail.com>
2016-07-05 16:08:30 -0700

    Add Austin MATLAB Files

---
+++ MATLAB - Austin/Prior_mov_GUI_Austin.m
@@ -0,0 +1,152 @@
+function varargout = Prior_mov_GUI_Austin(varargin)
+% PRIOR_MOV_GUI_AUSTIN MATLAB code for Prior_mov_GUI_Austin.fig
+%      PRIOR_MOV_GUI_AUSTIN, by itself, creates a new PRIOR_MOV_GUI_AUSTIN or raises the existing
+%      singleton*.
+%
+%      H = PRIOR_MOV_GUI_AUSTIN returns the handle to a new PRIOR_MOV_GUI_AUSTIN or the handle to
+%      the existing singleton*.
+%
+%      PRIOR_MOV_GUI_AUSTIN('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PRIOR_MOV_GUI_AUSTIN.M with the given input arguments.
+%
+%      PRIOR_MOV_GUI_AUSTIN('Property','Value',...) creates a new PRIOR_MOV_GUI_AUSTIN or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Prior_mov_GUI_Austin_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Prior_mov_GUI_Austin_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Prior_mov_GUI_Austin
+
+% Last Modified by GUIDE v2.5 05-Jul-2016 15:42:08
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Prior_mov_GUI_Austin_OpeningFcn, ...
+                   'gui_OutputFcn',  @Prior_mov_GUI_Austin_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Prior_mov_GUI_Austin is made visible.
+function Prior_mov_GUI_Austin_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Prior_mov_GUI_Austin (see VARARGIN)
+
+% Choose default command line output for Prior_mov_GUI_Austin
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Prior_mov_GUI_Austin wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Prior_mov_GUI_Austin_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in z_pos_prior.
+function z_pos_prior_Callback(hObject, eventdata, handles)
+% hObject    handle to z_pos_prior (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in z_neg_prior.
+function z_neg_prior_Callback(hObject, eventdata, handles)
+% hObject    handle to z_neg_prior (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in y_neg_prior.
+function y_neg_prior_Callback(hObject, eventdata, handles)
+% hObject    handle to y_neg_prior (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in y_pos_prior.
+function y_pos_prior_Callback(hObject, eventdata, handles)
+% hObject    handle to y_pos_prior (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in x_pos_prior.
+function x_pos_prior_Callback(hObject, eventdata, handles)
+% hObject    handle to x_pos_prior (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in x_neg_prior.
+function x_neg_prior_Callback(hObject, eventdata, handles)
+% hObject    handle to x_neg_prior (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in connect_to_controller.
+function connect_to_controller_Callback(hObject, eventdata, handles)
+% hObject    handle to connect_to_controller (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in disconnect_from_controller.
+function disconnect_from_controller_Callback(hObject, eventdata, handles)
+% hObject    handle to disconnect_from_controller (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function input_distance_Callback(hObject, eventdata, handles)
+% hObject    handle to input_distance (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of input_distance as text
+%        str2double(get(hObject,'String')) returns contents of input_distance as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function input_distance_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to input_distance (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
